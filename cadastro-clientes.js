befbadff0063585414eb4bf951d98a9b392cf8fe
luryrodrigues <lurylrodrigues@gmail.com>
2020-12-21 22:50:44 -0300

    CPF validation

--- cadastro-clientes.js
+++ cadastro-clientes.js
@@ -3,8 +3,12 @@ const formCadastroCliente = document.querySelector('[data-form]')
 formCadastroCliente.addEventListener("submit", event => {
   event.preventDefault()
 
-  const nome = event.target.querySelector("[data-nome]")
-  const cpf = event.target.querySelector("[data-cpf]")
+  const nome = event.target.querySelector("[data-nome]").value
+  const cpf = event.target.querySelector("[data-cpf]").value
 
-  cadastrarClientes(nome.value, cpf.value)
+    if (validaCPF(cpf)){
+      cadastrarClientes(nome, cpf)
+    } else {
+      alert('CPF inválido. Por favor, insira um CPF válido.')
+    }
 })
